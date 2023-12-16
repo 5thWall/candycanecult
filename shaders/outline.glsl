8e49dcdfb23f4dc296edeeed21bb50552f5dfbc4
Andy Arminio <andrew.arminio@gmail.com>
2023-12-16 11:31:44 -0700

    Outline objects in red

--- shaders/outline.glsl
+++ shaders/outline.glsl
@@ -7,5 +7,5 @@ vec4 effect(vec4 col, Image texture, vec2 texturePos, vec2 screenPos) {
   alpha -= texture2D(texture, texturePos + vec2(stepSize.x, 0.)).a;
   alpha -= texture2D(texture, texturePos + vec2(0.0, -stepSize.y)).a;
   alpha -= texture2D(texture, texturePos + vec2(0.0, stepSize.y)).a;
-  return vec4(vec3(1.), alpha);
+  return vec4(1., 0., 0., alpha);
 }
